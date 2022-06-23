5b52cac2da06a264b07c9335d2091152be307ff5
Aidan Holland <aidan@censys.io>
2022-06-23 13:17:04 -0400

    chore(cli): Temporarily hide AWS from config setup

--- src/censys/cloud_connectors/common/cli/commands/config.py
+++ src/censys/cloud_connectors/common/cli/commands/config.py
@@ -41,6 +41,8 @@ def cli_config(args: argparse.Namespace):
                         "value": provider,
                     }
                     for provider in ProviderEnum
+                    # TODO: Remove this once AWS is supported
+                    if provider != ProviderEnum.AWS
                 ],
             }
         ]
